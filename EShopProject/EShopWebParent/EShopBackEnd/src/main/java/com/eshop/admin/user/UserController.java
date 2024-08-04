acabea9f374f5d3ff603277fab8f2b4985aa4db8
tobezero01 <ducnhuad@gmail.com>
2024-08-04 20:39:37 +0700

    add user function message success

--- EShopProject/EShopWebParent/EShopBackEnd/src/main/java/com/eshop/admin/user/UserController.java
+++ EShopProject/EShopWebParent/EShopBackEnd/src/main/java/com/eshop/admin/user/UserController.java
@@ -7,11 +7,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.eshop.common.entity.Role;
 import com.eshop.common.entity.User;
-
-
 
 @Controller
 public class UserController {
@@ -37,8 +36,9 @@ public class UserController {
 	}
 	
 	@PostMapping("/users/save")
-	public String saveUser(User user) {
+	public String saveUser(User user, RedirectAttributes redirectAttributes) {
 		userService.save(user);
+		redirectAttributes.addFlashAttribute("message", "The user has been saved cuccessfully!");
 		return "redirect:/users";
 	}
 }
