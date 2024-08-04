179d4b8f3f1671d53d241ffbdc94c5e5238c0c38
tobezero01 <ducnhuad@gmail.com>
2024-08-04 22:08:15 +0700

    security password add user with encodePassword

---
+++ EShopProject/EShopWebParent/EShopBackEnd/src/main/java/com/eshop/admin/security/WebSecurityConfig.java
@@ -0,0 +1,33 @@
+package com.eshop.admin.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig {
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+    
+    
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+    	httpSecurity.authorizeHttpRequests(
+    		configuration -> 
+    					configuration
+    					.anyRequest()
+    					.permitAll()
+    					
+    	);
+    	
+    	return httpSecurity.build();
+    }
+}
--- EShopProject/EShopWebParent/EShopBackEnd/src/main/java/com/eshop/admin/user/UserService.java
+++ EShopProject/EShopWebParent/EShopBackEnd/src/main/java/com/eshop/admin/user/UserService.java
@@ -2,7 +2,9 @@ package com.eshop.admin.user;
 
 import java.util.List;
 
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.eshop.common.entity.Role;
@@ -17,6 +19,9 @@ public class UserService {
 	@Autowired
 	private RoleRepository roleRepository;
 	
+	@Autowired
+	private PasswordEncoder passwordEncoder;
+	
 	public List<User> listALl() {
 		return (List<User>) repository.findAll();
 	}
@@ -26,7 +31,13 @@ public class UserService {
 	}
 	
 	public void save(User user) {
+		encodePass(user);
 		repository.save(user);
+	}
+	
+	private void encodePass(User user) {
+		String encodePass = passwordEncoder.encode(user.getPassword());
+		user.setPassword(encodePass);
 	}
 	
 }
