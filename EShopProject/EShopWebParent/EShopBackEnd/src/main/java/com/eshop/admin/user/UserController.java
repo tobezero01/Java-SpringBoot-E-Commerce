586d8c25791577b4de054ba60b29b812fa2a51da
tobezero01 <ducnhuad@gmail.com>
2024-08-07 04:44:50 +0700

    pagination endpoint

--- EShopProject/EShopWebParent/EShopBackEnd/src/main/java/com/eshop/admin/user/UserController.java
+++ EShopProject/EShopWebParent/EShopBackEnd/src/main/java/com/eshop/admin/user/UserController.java
@@ -6,6 +6,7 @@ import java.util.Optional;
 
 import com.eshop.admin.FileUploadUtil;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.util.StringUtils;
@@ -27,11 +28,29 @@ public class UserController {
 	
 	@GetMapping("/users")
 	public String listAll(Model model) {
-		List<User> users = userService.listALl();
-		model.addAttribute("listUsers",users);
+		return listByPage(1, model);
+	}
+
+	@GetMapping("/users/page/{pageNum}")
+	public String listByPage(@PathVariable(name = "pageNum") int pageNum, Model model) {
+		Page<User> page = userService.listByPage(pageNum);
+		List<User> listUsers = page.getContent();
+		long startCount = (pageNum-1)*UserService.USER_PER_PAGE + 1;
+		long endCount = startCount + UserService.USER_PER_PAGE -1;
+
+		if(endCount > page.getTotalElements()) {
+			endCount = page.getTotalElements();
+		}
+		model.addAttribute("totalPages", page.getTotalPages());
+		model.addAttribute("currentPage",pageNum);
+		model.addAttribute("startCount",startCount);
+		model.addAttribute("endCount", endCount);
+		model.addAttribute("totalItems", page.getTotalElements());
+		model.addAttribute("listUsers", listUsers);
+
 		return "users";
 	}
-	
+
 	@GetMapping("/users/new")
 	public String newUser(Model model) {
 		List<Role> listRoles = userService.listRoles();
