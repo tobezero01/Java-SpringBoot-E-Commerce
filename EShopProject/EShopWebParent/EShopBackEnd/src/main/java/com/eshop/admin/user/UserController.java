718108dfab7f32550c5a8da95408415a1cedf42b
tobezero01 <ducnhuad@gmail.com>
2024-08-07 16:43:22 +0700

    sort user function

--- EShopProject/EShopWebParent/EShopBackEnd/src/main/java/com/eshop/admin/user/UserController.java
+++ EShopProject/EShopWebParent/EShopBackEnd/src/main/java/com/eshop/admin/user/UserController.java
@@ -7,6 +7,7 @@ import java.util.Optional;
 import com.eshop.admin.FileUploadUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.util.StringUtils;
@@ -28,12 +29,14 @@ public class UserController {
 	
 	@GetMapping("/users")
 	public String listAll(Model model) {
-		return listByPage(1, model);
+		return listByPage(1, model, "firstName", "asc");
 	}
 
 	@GetMapping("/users/page/{pageNum}")
-	public String listByPage(@PathVariable(name = "pageNum") int pageNum, Model model) {
-		Page<User> page = userService.listByPage(pageNum);
+	public String listByPage(@PathVariable(name = "pageNum") int pageNum, Model model,
+							 @Param("sortField" ) String sortField, @Param("sortDir") String sortDir
+							 ) {
+		Page<User> page = userService.listByPage(pageNum, sortField,sortDir);
 		List<User> listUsers = page.getContent();
 		long startCount = (pageNum-1)*UserService.USER_PER_PAGE + 1;
 		long endCount = startCount + UserService.USER_PER_PAGE -1;
@@ -41,12 +44,17 @@ public class UserController {
 		if(endCount > page.getTotalElements()) {
 			endCount = page.getTotalElements();
 		}
+
+		String reverseSortDir = sortDir.equals("asc") ? "desc" : "asc";
 		model.addAttribute("totalPages", page.getTotalPages());
 		model.addAttribute("currentPage",pageNum);
 		model.addAttribute("startCount",startCount);
 		model.addAttribute("endCount", endCount);
 		model.addAttribute("totalItems", page.getTotalElements());
 		model.addAttribute("listUsers", listUsers);
+		model.addAttribute("sortField", sortField);
+		model.addAttribute("sortDir", sortDir);
+		model.addAttribute("reverseSortDir", reverseSortDir);
 
 		return "users";
 	}
