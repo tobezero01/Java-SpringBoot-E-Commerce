476f9a8c70233b938e6c5352a51006ebdda2cf8d
tobezero01 <ducnhuad@gmail.com>
2024-08-08 03:36:56 +0700

    add security change

--- EShopProject/EShopWebParent/EShopBackEnd/src/main/java/com/eshop/admin/security/WebSecurityConfig.java
+++ EShopProject/EShopWebParent/EShopBackEnd/src/main/java/com/eshop/admin/security/WebSecurityConfig.java
@@ -2,8 +2,12 @@ package com.eshop.admin.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -16,18 +20,42 @@ public class WebSecurityConfig {
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
-    
-    
+
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return (web) -> web.ignoring().requestMatchers("/images/**", "/js/**");
+    }
+
+//    @Bean
+//    UserDetailsService userDetailsService() {
+//        return new EShopUserDetailsService();
+//    }
+//
+//    @Bean
+//    DaoAuthenticationProvider authenticationProvider() {
+//        DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
+//        authenticationProvider.setUserDetailsService(userDetailsService());
+//        authenticationProvider.setPasswordEncoder(passwordEncoder());
+//
+//        return authenticationProvider;
+//    }
+
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
     	httpSecurity.authorizeHttpRequests(
-    		configuration -> 
-    					configuration
-    					.anyRequest()
-    					.permitAll()
-    					
-    	);
-    	
+    		configuration -> configuration
+    					.anyRequest().authenticated()
+                )
+                .formLogin(
+                        form -> form
+                                .loginPage("/login")
+                                .usernameParameter("email")
+                                .permitAll()
+                );
+
+
     	return httpSecurity.build();
     }
+
+
 }
