9b4de931c55352ce2d70638817b310e3390bb3d7
tobezero01 <ducnhuad@gmail.com>
2024-08-11 19:57:55 +0700

    code and edit CategoryForm

--- EShopProject/EShopWebParent/EShopBackEnd/src/main/java/com/eshop/admin/category/CategoryController.java
+++ EShopProject/EShopWebParent/EShopBackEnd/src/main/java/com/eshop/admin/category/CategoryController.java
@@ -2,6 +2,7 @@ package com.eshop.admin.category;
 
 import com.eshop.common.entity.Category;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.Banner;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -22,4 +23,15 @@ public class CategoryController {
 
         return "categories/categories";
     }
+
+    @GetMapping("/categories/new")
+    public String newCategory(Model model) {
+        List<Category> listCategories = categoryService.listCategoriesUsedInForm();
+        model.addAttribute("category", new Category());
+        model.addAttribute("pageTitle", "Create new category");
+        model.addAttribute("listCategories",listCategories);
+        return "categories/category_form";
+    }
+
+
 }
--- EShopProject/EShopWebParent/EShopBackEnd/src/main/java/com/eshop/admin/category/CategoryService.java
+++ EShopProject/EShopWebParent/EShopBackEnd/src/main/java/com/eshop/admin/category/CategoryService.java
@@ -4,7 +4,9 @@ import com.eshop.common.entity.Category;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
 
 @Service
 public class CategoryService {
@@ -13,5 +15,37 @@ public class CategoryService {
 
     public List<Category> listAll() {
         return categoryRepository.findAll();
+    }
+
+    public List<Category> listCategoriesUsedInForm() {
+        List<Category> categoriesUsedInForm = new ArrayList<>();
+        Iterable<Category> categoriesInDB = categoryRepository.findAll();
+        for (Category category : categoriesInDB) {
+            if(category.getParent() == null) {
+                categoriesUsedInForm.add(new Category(category.getName()));
+                Set<Category> children = category.getChildren();
+
+                for (Category subCategory : children) {
+                    String name = "--" + subCategory.getName();
+                    categoriesUsedInForm.add(new Category(name));
+
+                    listChildren(categoriesUsedInForm,subCategory,1);
+                }
+            }
+        }
+        return categoriesUsedInForm;
+    }
+    public void listChildren(List<Category> categoriesUsedInForm, Category parent , int subLevel) {
+        int newSubLevel = subLevel+1;
+        Set<Category> children = parent.getChildren();
+        for (Category subCategory : children) {
+            String name = "";
+            for (int i = 0 ; i < newSubLevel ; i++) {
+                name+="--";
+            }
+            name += subCategory.getName();
+            categoriesUsedInForm.add(new Category(name));
+            listChildren(categoriesUsedInForm,subCategory, newSubLevel);
+        }
     }
 }
