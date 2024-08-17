9cd3aa3288623c682a810e3ad1d67af3749dc50d
tobezero01 <ducnhuad@gmail.com>
2024-08-17 17:18:51 +0700

    add product repository

---
+++ EShopProject/EShopWebParent/EShopBackEnd/src/main/java/com/eshop/admin/product/ProductRepository.java
@@ -0,0 +1,9 @@
+package com.eshop.admin.product;
+
+import com.eshop.common.entity.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Integer> {
+}
