03a6f9769e397c3813efeee042909e1fd472c59d
tobezero01 <ducnhuad@gmail.com>
2024-08-17 22:43:46 +0700

    add product list all service and controller

--- EShopProject/EShopWebParent/EShopBackEnd/src/main/java/com/eshop/admin/product/ProductController.java
+++ EShopProject/EShopWebParent/EShopBackEnd/src/main/java/com/eshop/admin/product/ProductController.java
@@ -2,8 +2,13 @@ package com.eshop.admin.product;
 
 import com.eshop.admin.brand.BrandService;
 import com.eshop.admin.category.CategoryService;
+import com.eshop.common.entity.Product;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 @Controller
 public class ProductController {
@@ -16,6 +21,13 @@ public class ProductController {
 
     @Autowired
     private BrandService brandService;
+
+    @GetMapping("/products")
+    public String listAll(Model model) {
+        List<Product> listProducts = productService.listAll();
+        model.addAttribute("listProducts" , listProducts);
+        return "products/products";
+    }
 
 
 }
--- EShopProject/EShopWebParent/EShopBackEnd/src/main/java/com/eshop/admin/product/ProductService.java
+++ EShopProject/EShopWebParent/EShopBackEnd/src/main/java/com/eshop/admin/product/ProductService.java
@@ -1,8 +1,11 @@
 package com.eshop.admin.product;
 
+import com.eshop.common.entity.Product;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
 @Transactional
@@ -10,4 +13,8 @@ public class ProductService {
 
     @Autowired
     private ProductRepository productRepository;
+
+    public List<Product> listAll() {
+        return productRepository.findAll();
+    }
 }
