847cf4cb104841986ee6fb20841cf33db3364b90
tobezero01 <ducnhuad@gmail.com>
2024-08-19 12:00:01 +0700

    update brand repo findAll

--- EShopProject/EShopWebParent/EShopBackEnd/src/main/java/com/eshop/admin/brand/BrandRepository.java
+++ EShopProject/EShopWebParent/EShopBackEnd/src/main/java/com/eshop/admin/brand/BrandRepository.java
@@ -7,6 +7,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface BrandRepository extends JpaRepository <Brand , Integer>{
     public Long countById(Integer id) ;
@@ -15,4 +17,7 @@ public interface BrandRepository extends JpaRepository <Brand , Integer>{
 
     @Query("Select b from Brand b where b.name LIKE %?1%")
     public Page<Brand> findAll(String keyWord, Pageable pageable);
+
+    @Query("Select NEW Brand(b.id, b.name) From Brand b Order By b.name ASC")
+    public List<Brand> findAll();
 }
