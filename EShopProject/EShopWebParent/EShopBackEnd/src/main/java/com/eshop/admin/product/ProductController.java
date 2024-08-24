f49fc3478db543a413e67cc6574ffa4f4b90394f
tobezero01 <ducnhuad@gmail.com>
2024-08-24 13:13:52 +0700

    add new product first code

--- EShopProject/EShopWebParent/EShopBackEnd/src/main/java/com/eshop/admin/product/ProductController.java
+++ EShopProject/EShopWebParent/EShopBackEnd/src/main/java/com/eshop/admin/product/ProductController.java
@@ -10,6 +10,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
 
@@ -50,7 +51,9 @@ public class ProductController {
     }
 
     @PostMapping("/products/save")
-    public String saveProduct() {
+    public String saveProduct(Product product , RedirectAttributes redirectAttributes) {
+        productService.save(product);
+        redirectAttributes.addFlashAttribute("message","The product has been saved successfully");
         return "redirect:/products";
     }
 
--- EShopProject/EShopWebParent/EShopBackEnd/src/main/java/com/eshop/admin/product/ProductService.java
+++ EShopProject/EShopWebParent/EShopBackEnd/src/main/java/com/eshop/admin/product/ProductService.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Date;
 import java.util.List;
 
 @Service
@@ -16,5 +17,21 @@ public class ProductService {
 
     public List<Product> listAll() {
         return productRepository.findAll();
+    }
+
+    public Product save(Product product) {
+        if(product.getId() == null) {
+            product.setCreatedTime(new Date());
+        }
+
+        if (product.getAlias() == null || product.getAlias().isEmpty()) {
+            String defaultAlias = product.getName().replace(" ", "-");
+            product.setAlias(defaultAlias);
+        } else {
+            product.setAlias(product.getName().replace(" ", "-"));
+        }
+
+        product.setUpdatedTime(new Date());
+        return productRepository.save(product);
     }
 }
