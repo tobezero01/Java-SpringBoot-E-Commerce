25fae75f163001c2317af567c850b88ae6e67173
tobezero01 <ducnhuad@gmail.com>
2024-08-29 01:57:14 +0700

    start coding modal detail (error ) fix later

--- EShopProject/EShopWebParent/EShopBackEnd/src/main/java/com/eshop/admin/product/ProductController.java
+++ EShopProject/EShopWebParent/EShopBackEnd/src/main/java/com/eshop/admin/product/ProductController.java
@@ -150,6 +150,22 @@ public class ProductController {
         }
     }
 
+
+    @GetMapping("/products/detail/{id}")
+    public String Product(@PathVariable("id") Integer id , Model model,
+                              RedirectAttributes redirectAttributes) {
+        try {
+            Product product = productService.get(id);
+
+            model.addAttribute("product" , product);
+
+            return "products/product_detail_modal";
+        } catch (ProductNotFoundException ex) {
+            redirectAttributes.addFlashAttribute("message" , ex.getMessage());
+            return "redirect:/products";
+        }
+    }
+
     @GetMapping("/products/{id}/enabled/{status}")
     public String updateCategoryEnabledStatus(@PathVariable("id") Integer id,
                                               @PathVariable("status") boolean enabled,
