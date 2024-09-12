ec6dbc6470c16b54826f41016b8a600fa30dd6e3
tobezero01 <ducnhuad@gmail.com>
2024-09-12 17:40:04 +0700

    add Currency enity , repo and unitTest

---
+++ EShopProject/EShopWebParent/EShopBackEnd/src/main/java/com/eshop/admin/setting/CurrencyRepository.java
@@ -0,0 +1,10 @@
+package com.eshop.admin.setting;
+
+import com.eshop.common.entity.Currency;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CurrencyRepository extends JpaRepository<Currency, Integer> {
+
+}
