e437fa6aa73031a123547ce9ade5ae7baf0337f0
tobezero01 <ducnhuad@gmail.com>
2024-09-19 21:14:45 +0700

    add dto and restController State

---
+++ EShopProject/EShopWebParent/EShopBackEnd/src/main/java/com/eshop/admin/setting/state/StateRestController.java
@@ -0,0 +1,38 @@
+package com.eshop.admin.setting.state;
+
+import com.eshop.common.entity.Country;
+import com.eshop.common.entity.State;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+public class StateRestController {
+
+    @Autowired
+    StateRepository stateRepository;
+
+    @GetMapping("/states/list_by_country/{id}")
+    public List<StateDTO> listByCountry(@PathVariable("id") Integer countryId) {
+        List<State> listStates = stateRepository.findByCountryOrderByNameAsc(new Country(countryId));
+        List<StateDTO> result= new ArrayList<>();
+        for (State state : listStates) {
+            result.add(new StateDTO(state.getId(), state.getName()));
+        }
+        return result;
+    }
+
+    @PostMapping("/states/save")
+    public String save(@RequestBody State state) {
+        State savedState = stateRepository.save(state);
+        return String.valueOf(savedState.getId());
+    }
+
+    @GetMapping("/states/delete/{id}")
+    public void delete(@PathVariable("id") Integer id) {
+        stateRepository.deleteById(id);
+    }
+
+}
